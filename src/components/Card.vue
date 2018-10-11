631f27bfcc56fdef950ad55eb1cf6caa6823b759
wwt2008 <wwt2008@gmail.com>
2018-10-11 23:21:32 +0300

    Fixed issue when data about today weather is absent in the response.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,15 +3,22 @@
         <div class="card-body">
             <div class="col mb-2 text-center">
                 <h3 class="weather-city mb-3">{{ weather.title }}</h3>
-                <img :src="getIcon(weatherData)" width="100">
-                <p>{{ weatherData.weather_state_name }}</p>
+                <div v-if="weatherData.the_temp">
+                    <img :src="getIcon(weatherData)" width="100">
+                    <p>{{ weatherData.weather_state_name }}</p>
+                </div>
+                <div v-else class="weather-nodata-icon">
+                    <i class="fa fa-meh-o" aria-hidden="true"></i>
+                </div>
+
             </div>
 
-            <div>
+            <div v-if="weatherData.the_temp">
                 Temp: {{ Math.round(weatherData.the_temp)}}°C<br>
                 Temp max: {{ Math.round(weatherData.max_temp)}}°C<br>
                 Temp min: {{ Math.round(weatherData.min_temp)}}°C<br>
             </div>
+            <div v-else>Unfortunately, we haven't data about today weather, but you can click and see info for other days</div>
 
         </div>
     </div>
